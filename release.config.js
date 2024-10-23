22f56cde7d0e5b818720715003768d53d2e59981
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-10-23 16:09:33 +0200

    feat: add release steps to workflow

---
+++ release.config.js
@@ -0,0 +1,20 @@
+const config = {
+  branches: ["main", { name: "feat/release-test", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["./dist"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
+
+module.exports = config;
